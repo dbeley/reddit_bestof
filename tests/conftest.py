22245911a12a953103b85b1596d0e4fce971c426
dbeley <6568955+dbeley@users.noreply.github.com>
2021-12-30 01:33:38 +0100

    Add unit tests.

---
+++ tests/conftest.py
@@ -0,0 +1,209 @@
+import pytest
+import pandas as pd
+import numpy as np
+
+
+@pytest.fixture
+def test_posts_dataframe():
+    data = pd.DataFrame(
+        np.array(
+            [
+                ["author1", 12, 10, "title1", "permalink1", "id1"],
+                ["author2", 20, -1, "title2", "permalink2", "id2"],
+                ["author3", 0, 10, "title3", "permalink3", "id3"],
+                ["author4", 20, 5, "title4", "permalink4", "id4"],
+                ["author4", 40, 5, "Forum Libre 123", "permalink4", "id4"],
+            ]
+        ),
+        columns=["author", "num_comments", "score", "title", "permalink", "id"],
+    )
+    data["num_comments"] = pd.to_numeric(data["num_comments"])
+    data["score"] = pd.to_numeric(data["score"])
+    yield data
+
+
+@pytest.fixture
+def test_simple_comments_dataframe():
+    data = pd.DataFrame(
+        np.array(
+            [
+                ["author1", 3, "body1", "permalink1", "id1"],
+                ["author2", 3, "body2", "permalink2", "id2"],
+                ["author3", 2, "body3", "permalink3", "id3"],
+                ["author4", -1, "body4", "permalink4", "id4"],
+                ["author5", 1, "body5", "permalink5", "id5"],
+            ]
+        ),
+        columns=["author", "score", "body", "permalink", "id"],
+    )
+    data["score"] = pd.to_numeric(data["score"])
+    yield data
+
+
+@pytest.fixture
+def test_score_comments_dataframe():
+    data = pd.DataFrame(
+        np.array(
+            [
+                ["author1", 30, "body1", "permalink1", "id1"],
+                ["author1", 90, "body1", "permalink1", "id2"],
+                ["author1", 3, "body1", "permalink1", "id3"],
+                ["author2", -10, "body2", "permalink2", "id4"],
+                ["author2", 0, "body2", "permalink2", "id5"],
+                ["author3", 2, "body3", "permalink3", "id6"],
+                ["author4", -10, "body4", "permalink4", "id7"],
+                ["author5", 1000, "body5", "permalink5", "id8"],
+                ["author5", -931, "body5", "permalink5", "id9"],
+            ]
+        ),
+        columns=["author", "score", "body", "permalink", "id"],
+    )
+    data["score"] = pd.to_numeric(data["score"])
+    yield data
+
+
+@pytest.fixture
+def test_amoureux_comments_dataframe():
+    data = pd.DataFrame(
+        np.array(
+            [
+                ["author1", 1, "body1", "permalink1", "id1", "t1_id4"],
+                ["author1", 1, "body2", "permalink2", "id2", "t1_id4"],
+                ["author1", 1, "body3", "permalink3", "id3", "t1_id4"],
+                ["author2", 1, "body4", "permalink4", "id4", "t1_id1"],
+                ["author1", 1, "body5", "permalink5", "id5", "t1_id6"],
+                ["author3", 1, "body6", "permalink6", "id6", "t1_id4"],
+                ["author3", 1, "body7", "permalink7", "id7", "t1_id4"],
+                ["author2", 1, "body8", "permalink8", "id8", "t1_id7"],
+            ]
+        ),
+        columns=["author", "score", "body", "permalink", "id", "parent"],
+    )
+    data["score"] = pd.to_numeric(data["score"])
+    yield data
+
+
+@pytest.fixture
+def test_qualite_comments_dataframe():
+    data = pd.DataFrame(
+        np.array(
+            [
+                ["author1", 1, "abcdefghijklmnopqrstuvwxyz", "permalink1", "id1"],
+                ["author1", 3, "abcdefghijklmnopqrstuvwxyz", "permalink2", "id2"],
+                ["author1", 8, "abcdefghijklmnopqrstuvwxyz", "permalink3", "id3"],
+                ["author1", 1, "abcdefghijklmnopqrstuvwxyz", "permalink1", "id1"],
+                ["author1", 9, "abcdefghijklmnopqrstuvwxyz", "permalink2", "id2"],
+                ["author1", 2, "abcdefghijklmnopqrstuvwxyz", "permalink3", "id3"],
+                ["author2", 1, "abcdefghijklmnopqrstuvwxyz", "permalink4", "id4"],
+                ["author2", 1, "abcdefghijklmnopqrstuvwxyz", "permalink5", "id5"],
+                ["author2", 1, "abcdefghijklmnopqrstuvwxyz", "permalink6", "id6"],
+                ["author3", 1, "abcdefghijklmnopqrstuvwxyz", "permalink7", "id7"],
+                ["author3", 1, "abcdefghijklmnopqrstuvwxyz", "permalink8", "id8"],
+                ["author3", 1, "abcdefghijklmnopqrstuvwxyz", "permalink8", "id8"],
+                ["author3", 1, "abcdefghijklmnopqrstuvwxyz", "permalink8", "id8"],
+                ["author3", 1, "abcdefghijklmnopqrstuvwxyz", "permalink8", "id8"],
+                ["author3", 1, "abcdefghijklmnopqrstuvwxyz", "permalink8", "id8"],
+            ]
+        ),
+        columns=["author", "score", "body", "permalink", "id"],
+    )
+    data["score"] = pd.to_numeric(data["score"])
+    data["length"] = data["body"].str.len()
+    yield data
+
+
+@pytest.fixture
+def test_poc_comments_dataframe():
+    data = pd.DataFrame(
+        np.array(
+            [
+                ["author1", 1, "body1", "permalink1", "id1", "t1_id4"],
+                ["author1", 1, "body2", "permalink2", "id2", "t1_id4"],
+                ["author1", 1, "body3", "permalink3", "id3", "t1_id4"],
+                ["author2", 1, "body4", "permalink4", "id4", "t1_id1"],
+                ["author1", 1, "body5", "permalink5", "id5", "t1_id6"],
+                ["author3", 1, "body6", "permalink6", "id6", "t1_id4"],
+                ["author3", 1, "body7", "permalink7", "id7", "t1_id4"],
+                ["author2", 1, "body8", "permalink8", "id8", "t1_id7"],
+            ]
+        ),
+        columns=["author", "score", "body", "permalink", "id", "parent"],
+    )
+    data["score"] = pd.to_numeric(data["score"])
+    yield data
+
+
+@pytest.fixture
+def test_tartine_comments_dataframe():
+    data = pd.DataFrame(
+        np.array(
+            [
+                ["author1", 1, "abcdefghijklmnopqrstuvwxyz", "permalink1", "id1"],
+                ["author1", 3, "abcdefghijklmnopqrstuvwxyz", "permalink2", "id2"],
+                ["author1", 8, "abcdefghijklmnopqrstuvwxyz", "permalink3", "id3"],
+                ["author1", 1, "abcdefghijklmnopqrstuvwxyz", "permalink1", "id1"],
+                ["author1", 9, "abcdefghijklmnopqrstuvwxyz", "permalink2", "id2"],
+                ["author1", 2, "abcdefghijklmnopqrstuvwxyz", "permalink3", "id3"],
+                ["author2", 1, "abcdefghijklmnopqrstuvwxyz", "permalink4", "id4"],
+                ["author2", 1, "abcdefghijklmnopqrstuvwxyz", "permalink5", "id5"],
+                ["author2", 1, "abcdefghijklmnopqrstuvwxyz", "permalink6", "id6"],
+                ["author3", 1, "abcdefghijklmnopqrstuvwxyz", "permalink7", "id7"],
+                ["author3", 1, "abcdefghijklmnopqrstuvwxyz", "permalink8", "id8"],
+                ["author3", 1, "abcdefghijklmnopqrstuvwxyz", "permalink8", "id8"],
+                ["author3", 1, "abcdefghijklmnopqrstuvwxyz", "permalink8", "id8"],
+                ["author3", 1, "abcdefghijklmnopqrstuvwxyz", "permalink8", "id8"],
+                ["author3", 1, "abcdefghijklmnopqrstuvwxyz", "permalink8", "id8"],
+            ]
+        ),
+        columns=["author", "score", "body", "permalink", "id"],
+    )
+    data["score"] = pd.to_numeric(data["score"])
+    data["length"] = data["body"].str.len()
+    yield data
+
+
+@pytest.fixture
+def test_capslock_comments_dataframe():
+    data = pd.DataFrame(
+        np.array(
+            [
+                [
+                    "author1",
+                    1,
+                    "Une phrase",
+                    "permalink1",
+                    "id1",
+                ],
+                ["author1", 3, "CECI COMPTE", "permalink2", "id2"],
+                ["author1", 3, "abcdefghijklmnopqrstuvwxyz", "permalink2", "id2"],
+                ["author2", 3, "abcdefghijklmnopqrstuvwxyz", "permalink2", "id2"],
+            ]
+        ),
+        columns=["author", "score", "body", "permalink", "id"],
+    )
+    data["score"] = pd.to_numeric(data["score"])
+    data["length"] = data["body"].str.len()
+    yield data
+
+
+@pytest.fixture
+def test_indecision_comments_dataframe():
+    data = pd.DataFrame(
+        np.array(
+            [
+                [
+                    "author1",
+                    3,
+                    "On en parle ??? C'est une question ?",
+                    "permalink2",
+                    "id2",
+                ],
+                ["author1", 3, "Quoi ?!!?!! Autre question!!!!!?", "permalink2", "id2"],
+                ["author2", 3, "?????????????????????????", "permalink2", "id2"],
+            ]
+        ),
+        columns=["author", "score", "body", "permalink", "id"],
+    )
+    data["score"] = pd.to_numeric(data["score"])
+    data["length"] = data["body"].str.len()
+    yield data
---
+++ tests/test_utils.py
@@ -0,0 +1,174 @@
+import pandas as pd
+import numpy as np
+import praw
+from reddit_bestof import utils
+
+
+def test_sanitize_comment_body():
+    comment_body = "> comment1\nline of text1\n> comment2\nline of text2"
+
+    comment_body_expected = "line of text1 line of text2"
+    assert utils.sanitize_comment_body(comment_body) == comment_body_expected
+
+
+def test_sanitize_long_text():
+    small_text = "abcdefg"
+    long_text = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyz"
+    long_text_expected = "abcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrstuvwxyzabcdefghijklmnopqrs…"
+    assert utils.sanitize_long_text(small_text) == small_text
+    assert utils.sanitize_long_text(long_text) == long_text_expected
+
+
+def test_sanitize_username():
+    unknown_username = "None"
+    unknown_username_expected = "un inconnu"
+
+    assert utils.sanitize_username(unknown_username) == unknown_username_expected
+
+
+def test_sanitize_link():
+    link = "/test"
+    link_expected = "https://reddit.com/test?context=2"
+
+    assert utils.sanitize_link(link) == link_expected
+
+
+def test_get_best_post(test_posts_dataframe):
+    expected_result = {
+        "best_post_author": "author1",
+        "best_post_score": 10,
+        "best_post_title": "title1",
+        "best_post_link": "permalink1",
+        "best_post_id": "id1",
+    }
+    print(utils.get_best_post(test_posts_dataframe))
+
+    assert utils.get_best_post(test_posts_dataframe) == expected_result
+
+
+def test_get_commented_post(test_posts_dataframe):
+    expected_result = {
+        "commented_post_author": "author2",
+        "commented_post_comments": 20,
+        "commented_post_title": "title2",
+        "commented_post_link": "permalink2",
+        "commented_post_id": "id2",
+    }
+    print(utils.get_commented_post(test_posts_dataframe))
+
+    assert utils.get_commented_post(test_posts_dataframe) == expected_result
+
+
+def test_get_best_comment(test_simple_comments_dataframe):
+    expected_result = {
+        "best_comment_author": "author1",
+        "best_comment_score": 3,
+        "best_comment_body": "body1",
+        "best_comment_link": "permalink1",
+        "best_comment_id": "id1",
+    }
+    print(utils.get_best_comment(test_simple_comments_dataframe))
+
+    assert utils.get_best_comment(test_simple_comments_dataframe) == expected_result
+
+
+def test_get_worst_comment(test_simple_comments_dataframe):
+    expected_result = {
+        "worst_comment_author": "author4",
+        "worst_comment_score": -1,
+        "worst_comment_body": "body4",
+        "worst_comment_link": "permalink4",
+        "worst_comment_id": "id4",
+    }
+    print(utils.get_worst_comment(test_simple_comments_dataframe))
+
+    assert utils.get_worst_comment(test_simple_comments_dataframe) == expected_result
+
+
+# def test_get_discussed_comment(test_simple_comments_dataframe):
+#     expected_result = {
+#         "discussed_comment_author": "author4",
+#         "discussed_comment_score": -1,
+#         "discussed_comment_body": "body4",
+#         "discussed_comment_link": "permalink4",
+#         "discussed_comment_id": "id4",
+#     }
+#     print(utils.get_discussed_comment(reddit, test_simple_comments_dataframe))
+
+#     assert (
+#         utils.get_discussed_comment(reddit, test_simple_comments_dataframe) == expected_result
+#     )
+
+
+def test_get_amoureux(test_amoureux_comments_dataframe):
+    expected_result = {
+        "amoureux_author1": "author1",
+        "amoureux_author2": "author2",
+        "amoureux_score": 4,
+    }
+    print(utils.get_amoureux(test_amoureux_comments_dataframe))
+
+    assert utils.get_amoureux(test_amoureux_comments_dataframe) == expected_result
+
+
+def test_get_qualite(test_qualite_comments_dataframe):
+    expected_result = {"qualite_author": "author1", "qualite_score": 153.85}
+
+    print(test_qualite_comments_dataframe)
+    print(utils.get_qualite(test_qualite_comments_dataframe))
+
+    assert utils.get_qualite(test_qualite_comments_dataframe) == expected_result
+
+
+def test_get_poc(test_poc_comments_dataframe):
+    expected_result = {"poc_author": "author1", "poc_score": 4}
+
+    print(test_poc_comments_dataframe)
+    print(utils.get_poc(test_poc_comments_dataframe))
+
+    assert utils.get_poc(test_poc_comments_dataframe) == expected_result
+
+
+def test_get_tartine(test_tartine_comments_dataframe):
+    expected_result = {"tartine_author": "author1", "tartine_score": 156}
+
+    print(test_tartine_comments_dataframe)
+    print(utils.get_tartine(test_tartine_comments_dataframe))
+
+    assert utils.get_tartine(test_tartine_comments_dataframe) == expected_result
+
+
+# def test_get_capslock(test_capslock_comments_dataframe):
+#     expected_result = {"capslock_author": "author1", "capslock_score": 156}
+
+#     print(test_capslock_comments_dataframe)
+#     print(utils.get_capslock(test_capslock_comments_dataframe))
+
+#     assert utils.get_capslock(test_capslock_comments_dataframe) == expected_result
+
+
+# def test_get_indecision(test_indecision_comments_dataframe):
+#     expected_result = {"indecision_author": "author1", "indecision_score": 156}
+
+#     print(test_indecision_comments_dataframe)
+#     print(utils.get_indecision(test_indecision_comments_dataframe))
+
+#     assert utils.get_indecision(test_indecision_comments_dataframe) == expected_result
+
+
+def test_get_jackpot(test_score_comments_dataframe):
+    expected_result = {"jackpot_author": "author1", "jackpot_score": 123}
+
+    print(test_score_comments_dataframe)
+    print(utils.get_jackpot(test_score_comments_dataframe))
+
+    assert utils.get_jackpot(test_score_comments_dataframe) == expected_result
+
+
+def test_get_krach(test_score_comments_dataframe):
+    expected_result = {"krach_author": "author2", "krach_score": -10}
+
+    print(test_score_comments_dataframe)
+    print(utils.get_krach(test_score_comments_dataframe))
+
+    assert utils.get_krach(test_score_comments_dataframe) == expected_result
