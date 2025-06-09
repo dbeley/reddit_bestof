d0c72b008eaeec8cd6a27e2f4125f5b17b610357
dbeley <6568955+dbeley@users.noreply.github.com>
2025-06-09 21:56:13 +0200

    fix tests

--- tests/test_timestamp_range.py
+++ tests/test_timestamp_range.py
@@ -9,7 +9,7 @@ def test_day_timestamp():
         int(datetime(2021, 11, 1, 23, 00).timestamp()),
         int(datetime(2021, 11, 2, 23, 00).timestamp()),
     )
-    assert get_timestamp_range("day", "2021-11-02") == result
+    assert get_timestamp_range("2021-11-02") == result
 
 
 def test_day_end_of_month_timestamp():
@@ -18,40 +18,4 @@ def test_day_end_of_month_timestamp():
         int(datetime(2021, 1, 31, 23, 00).timestamp()),
         int(datetime(2021, 2, 1, 23, 00).timestamp()),
     )
-    assert get_timestamp_range("day", "2021-02-01") == result
-
-
-def test_month_timestamp():
-    result = (
-        "du mois de mars 2021",
-        int(datetime(2021, 3, 1, 2, 00).timestamp()),
-        int(datetime(2021, 4, 1, 2, 00).timestamp()),
-    )
-    assert get_timestamp_range("month", "2021-03") == result
-
-
-def test_month_december_timestamp():
-    result = (
-        "du mois de déc. 2021",
-        int(datetime(2021, 12, 1, 2, 00).timestamp()),
-        int(datetime(2022, 1, 1, 2, 00).timestamp()),
-    )
-    assert get_timestamp_range("month", "2021-12") == result
-
-
-def test_month_january_timestamp():
-    result = (
-        "du mois de janv. 2022",
-        int(datetime(2022, 1, 1, 2, 00).timestamp()),
-        int(datetime(2022, 2, 1, 2, 00).timestamp()),
-    )
-    assert get_timestamp_range("month", "2022-01") == result
-
-
-def test_year_timestamp():
-    result = (
-        "de l'année 2021",
-        int(datetime(2021, 1, 1, 2, 00).timestamp()),
-        int(datetime(2022, 1, 1, 2, 00).timestamp()),
-    )
-    assert get_timestamp_range("year", "2021") == result
+    assert get_timestamp_range("2021-02-01") == result
